c0abc201b91a2dc65297fd84a412832f9fdc3024
realjay2 <ajtheprince2020@gmail.com>
2026-04-18 23:16:58 -0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,2 +1,2 @@
-local content = HttpGet("https://raw.githubusercontent.com/realjay2/QuantV-Status/refs/heads/main/dump.luae") --use this to fetch url link
+local content = HttpGet("https://raw.githubusercontent.com/realjay2/QuantV-Status/refs/heads/main/dump.lua") --use this to fetch url link
 local response = HttpRequest("https://raw.githubusercontent.com/realjay2/QuantV-Status/refs/heads/main/dump.lua", "POST", {["Content-Type"] = "application/json"}, {key = "value"}) --use this to request api
